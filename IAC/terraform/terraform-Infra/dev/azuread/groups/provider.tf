01107d42e7faf6079f019e2309428ce750c49e90
VIVEK-cogniwide <vivek.senthilkumar@cogniwide.com>
2024-12-03 17:09:55 +0530

    commit-8

--- IAC/terraform/terraform-Infra/dev/azuread/groups/provider.tf
+++ IAC/terraform/terraform-Infra/dev/azuread/groups/provider.tf
@@ -1,7 +1,8 @@
-provider "azurerm" {
-    
-    features {
-      
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.41.0"
     }
-  
+  }
 }
