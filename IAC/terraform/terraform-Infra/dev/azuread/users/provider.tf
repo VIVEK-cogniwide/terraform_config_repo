892b527cea4df5397465055105f4e464af9782da
VIVEK-cogniwide <vivek.senthilkumar@cogniwide.com>
2024-12-04 11:01:42 +0530

    commit-10

--- IAC/terraform/terraform-Infra/dev/azuread/users/provider.tf
+++ IAC/terraform/terraform-Infra/dev/azuread/users/provider.tf
@@ -1,7 +1,8 @@
-provider "azuread" {
-  
-  client_id     = var.client_id
-  client_secret = var.client_secret
-  tenant_id     = var.tenant_id
-  
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.41.0"
+    }
+  }
 }
